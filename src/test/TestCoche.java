02ebc23c3ad76a9f3cd13cd6088024a51268974d
Hector <caminitodejerez@hotmail.com>
2023-03-30 20:40:07 +0200

    Refactorizado

--- src/test/TestCoche.java
+++ src/test/TestCoche.java
@@ -15,21 +15,21 @@ class TestCoche {
 	@Test
 	void test_al_acelerar_un_coche_su_velocidad_aumenta_Hector() {
 		Coche nuevoCoche = new Coche();
-		nuevoCoche.acelerar(30);
+		nuevoCoche.acelerar_Hector(30);
         Assertions.assertEquals(30,nuevoCoche.velocidad);
 	}
 	@Test
 	void test_al_decelerar_un_coche_su_velocidad_disminuye_Hector() {
 		  Coche nuevoCoche = new Coche();
 	        nuevoCoche.velocidad = 50;
-	        nuevoCoche.decelerar(20);
+	        nuevoCoche.decelerar_Hector(20);
 	        Assertions.assertEquals(30,nuevoCoche.velocidad);
 	}
 	 @Test
 	    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_de_cero_Hector(){
 	        Coche nuevoCoche = new Coche();
 	        nuevoCoche.velocidad = 50;
-	        nuevoCoche.decelerar(80);
+	        nuevoCoche.decelerar_Hector(80);
 	        Assertions.assertEquals(0,nuevoCoche.velocidad);
 	    }
 
